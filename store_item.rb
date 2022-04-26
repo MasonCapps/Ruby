cf6ecc01c0634e2590941851219c35590a46a95a
Mason Capps <masoncapps42119@gmail.com>
2022-04-26 15:58:58 -0500

    Created a class for items

--- store_item.rb
+++ store_item.rb
@@ -11,3 +11,37 @@ item3 = { :name => "carpet", :color => "brown", :price => 20 }
 puts "#{item1[:name]} is the color #{item1[:color]} and costs #{item1[:price]} dollars."
 puts "#{item2[:name]} is the color #{item2[:color]} and costs #{item2[:price]} dollars."
 puts "#{item3[:name]} is the color #{item3[:color]} and costs #{item3[:price]} dollars."
+
+#Hash As A Class
+class Item
+  def initialize(name, color, price)
+    @name = name
+    @color = color
+    @price = price
+  end
+
+  def name
+    @name
+  end
+
+  def color
+    @color
+  end
+
+  def price
+    @price
+  end
+
+  def information
+    puts "#{@name} is the color #{@color} and costs #{@price} dollars."
+  end
+
+  def price=(price_change)
+    @price = price_change
+  end
+end
+
+item = Item.new("Water", "clear", 2)
+item.information
+item.price = 5
+item.information
