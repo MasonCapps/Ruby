686b23a79b9ffdd3094cba07fc5b135efa0a5caa
Mason Capps <masoncapps42119@gmail.com>
2022-04-26 13:59:24 -0500

    Hashes using Ruby Symbol Syntax

--- store_item.rb
+++ store_item.rb
@@ -2,3 +2,8 @@
 item1 = { name: water, color: clear, price: 2 }
 item2 = { name: candle, color: blue, price: 10 }
 item3 = { name: carpet, color: brown, price: 20 }
+
+#Ruby Symbol Syntax
+item1 = { :name => water, :color => clear, :price => 2 }
+item2 = { :name => candle, :color => blue, :price => 10 }
+item3 = { :name => carpet, :color => brown, :price => 20 }
